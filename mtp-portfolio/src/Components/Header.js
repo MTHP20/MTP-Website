4426a583fe6320120c1b79a10bec244bf3eb031f
Michael Panlilio <mtpanlilio20@hotmail.com>
2024-05-31 19:59:43 +0100

    Starting new with React.js

---
+++ mtp-portfolio/src/Components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Your Name</h1>
+      <nav>
+        <ul>
+          <li><a href="#home">Home</a></li>
+          <li><a href="#projects">Projects</a></li>
+          <li><a href="#skills">Skills</a></li>
+          <li><a href="#contact">Contact</a></li>
+          <li><a href="#blog">Blog</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
